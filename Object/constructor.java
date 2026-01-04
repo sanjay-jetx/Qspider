8bf3cbed82f9c1edb64d9e110de392c2f5085965
sanjay-jetx <sanjaynathiya81@gmail.com>
2026-01-04 14:31:24 +0530

    Updated latest file

--- Object/constructor.java
+++ Object/constructor.java
@@ -1,6 +1,7 @@
 public class constructor {
     public static void main(String[] args) {
-        methos_name obj1 = new methos_name(12,"sanjay",12000);
+        methos_name obj1 = new methos_name(12, "sanjay", 5000000);
+        methos_name obj2 = new methos_name(11,"sanjay",6000000);
 
     }
 }
