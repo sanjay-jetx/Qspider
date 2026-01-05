202bc9b67c9db6bb57eff486fe932740fd48c91a
sanjay-jetx <sanjaynathiya81@gmail.com>
2026-01-05 20:14:14 +0530

    Updated latest file

---
+++ Binarysearch/peak.java
@@ -0,0 +1,25 @@
+package Binarysearch;
+
+public class peak {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 9, 14, 16, 18 };
+        int target = 15;
+        System.out.println(basic(arr, target));
+    }
+
+    static int basic(int[] a, int target) {
+        int lh = 0;
+        int rh = a.length - 1;
+        while (lh <= rh) {
+            int mid = (lh + rh) / 2;
+            if (a[mid] == target) {
+                return mid;
+            } else if (a[mid] < target) {
+                lh = mid + 1;
+            } else {
+                rh = mid - 1;
+            }
+        }
+        return lh;
+    }
+}
