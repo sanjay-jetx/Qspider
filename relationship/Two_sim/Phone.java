a57dee6373bdc6ec4183b48160f264ee3928b18e
sanjay-jetx <sanjaynathiya81@gmail.com>
2026-01-20 21:17:46 +0530

    your message

--- relationship/Two_sim/Phone.java
+++ relationship/Two_sim/Phone.java
@@ -2,14 +2,18 @@ package relationship.Two_sim;
 
 public class Phone {
     int prize;
-    Sim S_obj[];
-    int count = 0;
-    
-    public void add(String op, int bal) {
-        if(count == 2){
-            System.out.println("it is full ");
+    Sim S_obj[] = new Sim[2];
+
+    public void add(String operators, int balance) {
+        for (int i = 0; i < S_obj.length; i++) {
+            if (S_obj[i] == null) {
+                S_obj[i] = new Sim();
+                S_obj[i].operators = operators;
+                S_obj[i].balance = balance;
+                return;
+            }
         }
-        S_obj[count] = new Sim();
-        S_obj.Sim 
+        System.out.println("it is full");
+
     }
 }
