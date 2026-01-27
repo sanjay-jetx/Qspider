50cfe6c4f9e1bebb6cfb346abcdbd5c6730363e2
sanjay-jetx <sanjaynathiya81@gmail.com>
2026-01-27 21:51:30 +0530

    your message

---
+++ String/Reverse.java
@@ -0,0 +1,12 @@
+public class Reverse{
+    public static void main(String[] args) {
+        String name = "sanjay";
+        String rev = "";
+
+        for (int i = name.length() - 1; i >= 0; i--) {
+            rev = rev + name.charAt(i);
+        }
+
+        System.out.println(rev);
+    }
+}
